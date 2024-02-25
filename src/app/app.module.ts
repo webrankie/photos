d570840eaf86e77c5219411dae866dfb0c982eac
andreymyqa <andrey@mytime.com>
2024-02-24 18:59:23 -0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowPhotoComponent } from './show-photo/show-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowPhotoComponent
   ],
   imports: [
     BrowserModule,
